62d931dbea091cbc517f0ade246656f25460e370
Utpal19 <utpals_21@yahoo.com>
2023-12-12 00:19:28 +0530

    Adding project CMake file

--- main/webpage/app.js
+++ main/webpage/app.js
@@ -3,23 +3,22 @@
  */
 var seconds 	= null;
 var otaTimerVar =  null;
-var wifiConnectInterval = null;
+// var wifiConnectInterval = null;
 
 /**
  * Initialize functions here.
  */
 $(document).ready(function(){
-	getSSID();
+	// getSSID();
 	getUpdateStatus();
-	startDHTSensorInterval();
-	startLocalTimeInterval();
-	getConnectInfo();
-	$("#connect_wifi").on("click", function(){
-		checkCredentials();
-	}); 
-	$("#disconnect_wifi").on("click", function(){
-		disconnectWifi();
-	}); 
+	// startLocalTimeInterval();
+	// getConnectInfo();
+	// $("#connect_wifi").on("click", function(){
+		// checkCredentials();
+	// }); 
+	// $("#disconnect_wifi").on("click", function(){
+		// disconnectWifi();
+	// }); 
 });   
 
 /**
@@ -65,17 +64,17 @@ function updateFirmware()
 /**
  * Progress on transfers from the server to the client (downloads).
  */
-function updateProgress(oEvent) 
-{
-    if (oEvent.lengthComputable) 
-	{
-        getUpdateStatus();
-    } 
-	else 
-	{
-        window.alert('total size is unknown')
-    }
-}
+// function updateProgress(oEvent) 
+// {
+//     if (oEvent.lengthComputable) 
+// 	{
+//         getUpdateStatus();
+//     } 
+// 	else 
+// 	{
+//         window.alert('total size is unknown')
+//     }
+// }
 
 /**
  * Posts the firmware udpate status.
@@ -127,230 +126,192 @@ function otaRebootTimer()
     }
 }
 
-/**
- * Gets DHT22 sensor temperature and humidity values for display on the web page.
- */
-function getDHTSensorValues()
-{
-	$.getJSON('/dhtSensor.json', function(data) {
-		$("#temperature_reading").text(data["temp"]);
-		$("#humidity_reading").text(data["humidity"]);
-	});
-}
 
-/**
- * Sets the interval for getting the updated DHT22 sensor values.
- */
-function startDHTSensorInterval()
-{
-	setInterval(getDHTSensorValues, 5000);    
-}
+// /**
+//  * Clears the connection status interval.
+//  */
+// function stopWifiConnectStatusInterval()
+// {
+// 	if (wifiConnectInterval != null)
+// 	{
+// 		clearInterval(wifiConnectInterval);
+// 		wifiConnectInterval = null;
+// 	}
+// }
 
-/**
- * Clears the connection status interval.
- */
-function stopWifiConnectStatusInterval()
-{
-	if (wifiConnectInterval != null)
-	{
-		clearInterval(wifiConnectInterval);
-		wifiConnectInterval = null;
-	}
-}
-
-/**
- * Gets the WiFi connection status.
- */
-function getWifiConnectStatus()
-{
-	var xhr = new XMLHttpRequest();
-	var requestURL = "/wifiConnectStatus";
-	xhr.open('POST', requestURL, false);
-	xhr.send('wifi_connect_status');
+// /**
+//  * Gets the WiFi connection status.
+//  */
+// function getWifiConnectStatus()
+// {
+// 	var xhr = new XMLHttpRequest();
+// 	var requestURL = "/wifiConnectStatus";
+// 	xhr.open('POST', requestURL, false);
+// 	xhr.send('wifi_connect_status');
 	
-	if (xhr.readyState == 4 && xhr.status == 200)
-	{
-		var response = JSON.parse(xhr.responseText);
+// 	if (xhr.readyState == 4 && xhr.status == 200)
+// 	{
+// 		var response = JSON.parse(xhr.responseText);
 		
-		document.getElementById("wifi_connect_status").innerHTML = "Connecting...";
+// 		document.getElementById("wifi_connect_status").innerHTML = "Connecting...";
 		
-		if (response.wifi_connect_status == 2)
-		{
-			document.getElementById("wifi_connect_status").innerHTML = "<h4 class='rd'>Failed to Connect. Please check your AP credentials and compatibility</h4>";
-			stopWifiConnectStatusInterval();
-		}
-		else if (response.wifi_connect_status == 3)
-		{
-			document.getElementById("wifi_connect_status").innerHTML = "<h4 class='gr'>Connection Success!</h4>";
-			stopWifiConnectStatusInterval();
-			getConnectInfo();
-		}
-	}
-}
+// 		if (response.wifi_connect_status == 2)
+// 		{
+// 			document.getElementById("wifi_connect_status").innerHTML = "<h4 class='rd'>Failed to Connect. Please check your AP credentials and compatibility</h4>";
+// 			stopWifiConnectStatusInterval();
+// 		}
+// 		else if (response.wifi_connect_status == 3)
+// 		{
+// 			document.getElementById("wifi_connect_status").innerHTML = "<h4 class='gr'>Connection Success!</h4>";
+// 			stopWifiConnectStatusInterval();
+// 			getConnectInfo();
+// 		}
+// 	}
+// }
 
-/**
- * Starts the interval for checking the connection status.
- */
-function startWifiConnectStatusInterval()
-{
-	wifiConnectInterval = setInterval(getWifiConnectStatus, 2800);
-}
+// /**
+//  * Starts the interval for checking the connection status.
+//  */
+// function startWifiConnectStatusInterval()
+// {
+// 	wifiConnectInterval = setInterval(getWifiConnectStatus, 2800);
+// }
 
-/**
- * Connect WiFi function called using the SSID and password entered into the text fields.
- */
-function connectWifi()
-{
-	// Get the SSID and password
-	selectedSSID = $("#connect_ssid").val();
-	pwd = $("#connect_pass").val();
+// /**
+//  * Connect WiFi function called using the SSID and password entered into the text fields.
+//  */
+// function connectWifi()
+// {
+// 	// Get the SSID and password
+// 	selectedSSID = $("#connect_ssid").val();
+// 	pwd = $("#connect_pass").val();
 	
-	$.ajax({
-		url: '/wifiConnect.json',
-		dataType: 'json',
-		method: 'POST',
-		cache: false,
-		headers: {'my-connect-ssid': selectedSSID, 'my-connect-pwd': pwd},
-		data: {'timestamp': Date.now()}
-	});
+// 	$.ajax({
+// 		url: '/wifiConnect.json',
+// 		dataType: 'json',
+// 		method: 'POST',
+// 		cache: false,
+// 		headers: {'my-connect-ssid': selectedSSID, 'my-connect-pwd': pwd},
+// 		data: {'timestamp': Date.now()}
+// 	});
 	
-	startWifiConnectStatusInterval();
-}
+// 	startWifiConnectStatusInterval();
+// }
 
-/**
- * Checks credentials on connect_wifi button click.
- */
-function checkCredentials()
-{
-	errorList = "";
-	credsOk = true;
+// /**
+//  * Checks credentials on connect_wifi button click.
+//  */
+// function checkCredentials()
+// {
+// 	errorList = "";
+// 	credsOk = true;
 	
-	selectedSSID = $("#connect_ssid").val();
-	pwd = $("#connect_pass").val();
+// 	selectedSSID = $("#connect_ssid").val();
+// 	pwd = $("#connect_pass").val();
 	
-	if (selectedSSID == "")
-	{
-		errorList += "<h4 class='rd'>SSID cannot be empty!</h4>";
-		credsOk = false;
-	}
-	if (pwd == "")
-	{
-		errorList += "<h4 class='rd'>Password cannot be empty!</h4>";
-		credsOk = false;
-	}
+// 	if (selectedSSID == "")
+// 	{
+// 		errorList += "<h4 class='rd'>SSID cannot be empty!</h4>";
+// 		credsOk = false;
+// 	}
+// 	if (pwd == "")
+// 	{
+// 		errorList += "<h4 class='rd'>Password cannot be empty!</h4>";
+// 		credsOk = false;
+// 	}
 	
-	if (credsOk == false)
-	{
-		$("#wifi_connect_credentials_errors").html(errorList);
-	}
-	else
-	{
-		$("#wifi_connect_credentials_errors").html("");
-		connectWifi();    
-	}
-}
+// 	if (credsOk == false)
+// 	{
+// 		$("#wifi_connect_credentials_errors").html(errorList);
+// 	}
+// 	else
+// 	{
+// 		$("#wifi_connect_credentials_errors").html("");
+// 		connectWifi();    
+// 	}
+// }
 
-/**
- * Shows the WiFi password if the box is checked.
- */
-function showPassword()
-{
-	var x = document.getElementById("connect_pass");
-	if (x.type === "password")
-	{
-		x.type = "text";
-	}
-	else
-	{
-		x.type = "password";
-	}
-}
+// /**
+//  * Shows the WiFi password if the box is checked.
+//  */
+// function showPassword()
+// {
+// 	var x = document.getElementById("connect_pass");
+// 	if (x.type === "password")
+// 	{
+// 		x.type = "text";
+// 	}
+// 	else
+// 	{
+// 		x.type = "password";
+// 	}
+// }
 
-/**
- * Gets the connection information for displaying on the web page.
- */
-function getConnectInfo()
-{
-	$.getJSON('/wifiConnectInfo.json', function(data)
-	{
-		$("#connected_ap_label").html("Connected to: ");
-		$("#connected_ap").text(data["ap"]);
+// /**
+//  * Gets the connection information for displaying on the web page.
+//  */
+// function getConnectInfo()
+// {
+// 	$.getJSON('/wifiConnectInfo.json', function(data)
+// 	{
+// 		$("#connected_ap_label").html("Connected to: ");
+// 		$("#connected_ap").text(data["ap"]);
 		
-		$("#ip_address_label").html("IP Address: ");
-		$("#wifi_connect_ip").text(data["ip"]);
+// 		$("#ip_address_label").html("IP Address: ");
+// 		$("#wifi_connect_ip").text(data["ip"]);
 		
-		$("#netmask_label").html("Netmask: ");
-		$("#wifi_connect_netmask").text(data["netmask"]);
+// 		$("#netmask_label").html("Netmask: ");
+// 		$("#wifi_connect_netmask").text(data["netmask"]);
 		
-		$("#gateway_label").html("Gateway: ");
-		$("#wifi_connect_gw").text(data["gw"]);
+// 		$("#gateway_label").html("Gateway: ");
+// 		$("#wifi_connect_gw").text(data["gw"]);
 		
-		document.getElementById('disconnect_wifi').style.display = 'block';
-	});
-}
+// 		document.getElementById('disconnect_wifi').style.display = 'block';
+// 	});
+// }
 
-/**
- * Disconnects Wifi once the disconnect button is pressed and reloads the web page.
- */
-function disconnectWifi()
-{
-	$.ajax({
-		url: '/wifiDisconnect.json',
-		dataType: 'json',
-		method: 'DELETE',
-		cache: false,
-		data: { 'timestamp': Date.now() }
-	});
-	// Update the web page
-	setTimeout("location.reload(true);", 2000);
-}
+// /**
+//  * Disconnects Wifi once the disconnect button is pressed and reloads the web page.
+//  */
+// function disconnectWifi()
+// {
+// 	$.ajax({
+// 		url: '/wifiDisconnect.json',
+// 		dataType: 'json',
+// 		method: 'DELETE',
+// 		cache: false,
+// 		data: { 'timestamp': Date.now() }
+// 	});
+// 	// Update the web page
+// 	setTimeout("location.reload(true);", 2000);
+// }
 
-/**
- * Sets the interval for displaying local time.
- */
-function startLocalTimeInterval()
-{
-	setInterval(getLocalTime, 10000);
-}
+// /**
+//  * Sets the interval for displaying local time.
+//  */
+// function startLocalTimeInterval()
+// {
+// 	setInterval(getLocalTime, 10000);
+// }
 
-/**
- * Gets the local time.
- * @note connect the ESP32 to the internet and the time will be updated.
- */
-function getLocalTime()
-{
-	$.getJSON('/localTime.json', function(data) {
-		$("#local_time").text(data["time"]);
-	});
-}
+// /**
+//  * Gets the local time.
+//  * @note connect the ESP32 to the internet and the time will be updated.
+//  */
+// function getLocalTime()
+// {
+// 	$.getJSON('/localTime.json', function(data) {
+// 		$("#local_time").text(data["time"]);
+// 	});
+// }
 
-/**
- * Gets the ESP32's access point SSID for displaying on the web page.
- */
-function getSSID()
-{
-	$.getJSON('/apSSID.json', function(data) {
-		$("#ap_ssid").text(data["ssid"]);
-	});
-}
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-    
-
+// /**
+//  * Gets the ESP32's access point SSID for displaying on the web page.
+//  */
+// function getSSID()
+// {
+// 	$.getJSON('/apSSID.json', function(data) {
+// 		$("#ap_ssid").text(data["ssid"]);
+// 	});
+// }
 
